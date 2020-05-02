669998137263e0b739fbd7ba247650eff0f38537
badari07 <badari0793@gmail.com>
2020-05-03 01:14:47 +0530

    feat: setup react & initial components

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { Landing } from "./components/layout/Landing";
+import { Navbar } from "./components/layout/Navbar";
+
+const App = () => (
+  <>
+    <Navbar />
+    <Landing />
+  </>
+);
+
+export default App;
